eccbb1d857f68cf7265fb04204e5c2fd8987aeec
haidarhmd1 <haidar.hmd1@gmail.com>
2026-04-14 10:07:15 +0200

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,18 +1,12 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-dotenv.config({
-  path: process.env.NODE_ENV === "production" ? ".env" : ".env.local",
-});
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set.");
-}
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
